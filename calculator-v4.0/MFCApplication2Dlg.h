32bae8175a893b20f84afaf277f9687b9c60c9e4
jiuweilinghu <1354895960@qq.com>
2017-08-23 11:22:47 +0800

    计算器界面半成品

---
+++ calculator-v4.0/MFCApplication2Dlg.h
@@ -0,0 +1,55 @@
+
+// MFCApplication2Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMFCApplication2Dlg 对话框
+class CMFCApplication2Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCApplication2Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton32();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton29();
+	afx_msg void OnBnClickedButton6();
+	afx_msg void OnBnClickedButton30();
+	afx_msg void OnBnClickedButton31();
+	afx_msg void OnBnClickedButton10();
+	afx_msg void OnBnClickedButton33();
+	afx_msg void OnBnClickedButton12();
+	afx_msg void OnBnClickedButton14();
+	afx_msg void OnBnClickedButton15();
+	afx_msg void OnBnClickedButton39();
+	afx_msg void OnBnClickedButton18();
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnBnClickedButton9();
+	afx_msg void OnBnClickedButton13();
+	afx_msg void OnBnClickedButton17();
+	afx_msg void OnBnClickedButton21();
+	afx_msg void OnBnClickedButton20();
+	afx_msg void OnBnClickedButton16();
+};
