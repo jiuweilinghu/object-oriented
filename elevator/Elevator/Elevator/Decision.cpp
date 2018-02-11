c1f4aea45df70079e55dede4da64651747e18a26
jiuweilinghu <1354895960@qq.com>
2018-02-11 15:31:33 +0800

    修改部分函数

--- elevator/Elevator/Elevator/Decision.cpp
+++ elevator/Elevator/Elevator/Decision.cpp
@@ -3,6 +3,13 @@
 #include<iostream>
 using namespace std;
 
+int state[5][4];
+int time = 0;
+int direction = 0;  // 当前电梯方向
+int upNum = 0, dwNum = 0;
+int upCost = 0, dwCost = 0;
+
+int clock = 0;         // 模拟时钟
 
 // 交换函数，排序时用到
 void exchange(int array[][3], int a, int b)
@@ -32,13 +39,13 @@ void sort(int array[][3])
 }
 
 // 判断所有人是否都已经到达
-bool isfinished(int arrived[5])
+bool isfinished()
 {
 	int i = 0;
 
 	for ( i = 0; i < 5; i++)
 	{
-		if (arrived[i] == 0)
+		if (state[i][3]!=-1)
 			return false;
 	}
 
@@ -46,48 +53,7 @@ bool isfinished(int arrived[5])
 		return true;
 }
 
-// 判断该楼层是否有人进
-bool isin(int request[][3],int floor)
-{
-	int i = 0;
-	for ( i = 0; i < 5; i++)
-	{
-		if (floor == request[i][1])
-			return true;
-	}
-	if (i == 5)
-		return false;
-}
-
-// 判断该楼层是否有人进
-bool isout(int request[][3], int floor)
-{
-	int i = 0;
-	for (i = 0; i < 5; i++)
-	{
-		if ((request[i][2]==0 && floor == 10)
-			||(request[i][2]==1 && floor == 1))
-			return true;
-	}
-	if (i == 5)
-		return false;
-}
-
-// 判断该时刻是否有请求
-bool iswaiting(int waiting[11])
-{
-	int i = 1;
-	for (i = 1; i < 11; i++)
-	{
-		if (waiting[i] == 1|| waiting[i] == -1)
-			return true;
-	}
-	if (i == 11)
-		return false;
-}
-
-int num = 0;
-void update(int request[][3],int waiting[11],int clock)
+void update()
 {
 	// 设置等待状态：
 	// 0为该楼层没有请求
@@ -95,181 +61,216 @@ void update(int request[][3],int waiting[11],int clock)
 	// -1为该楼层有向下的请求
 	// 2代表该楼层向上的乘客已经进入电梯
 	// -2代表该楼层向下的乘客已经进入电梯
-	if (clock == request[num][0])
+	for (int i = 0; i < 5; i++)
 	{
-		if (request[num][2] == 1 && waiting[request[num][1]] == 0)
-			waiting[request[num][1]] = -1;
-		else if (request[num][2] == 0 && waiting[request[num][1]] == 0)
-			waiting[request[num][1]] = 1;
-		num++;
+		if (clock == state[i][0] && state[i][3] == 0)
+		{
+			state[i][3] = 1;
+		}
 	}
 }
-//请求时刻 起始楼层数 去往方向
+
+// 判断该楼层是否要停止
+void isStop(int floor)
+{
+	bool stop = false;
+	int i = 0;
+	int temp;
+
+	temp = clock;
+
+	for ( i = 0; i < 5; i++)
+	{
+		if (state[i][3] == 1 && floor == state[i][1]  )
+		{
+			state[i][3] = 0;
+			stop = true;
+			if (state[i][2] == 0)
+			{
+				upNum++;
+				if (floor != 10)
+				{
+					clock++;
+					update();
+				}
+			}
+			else
+			{
+				dwNum++;
+				if (floor != 1)
+				{
+					clock++;
+					update();
+				}
+			}
+		}
+	}
+	
+	if (floor == 10&&upNum>0)
+	{
+		upNum = 0;
+		stop = true;
+		for (int i = 0; i<5; i++)
+		{
+			if (state[i][3] == 0 && state[i][0]<clock&&state[i][2] == 0)
+			{
+				time += clock - state[i][0];
+				state[i][3] = -1;
+			}
+		}
+		clock++;
+		update();
+	}
+
+	else if (floor == 1 && dwNum>0)
+	{
+		dwNum = 0;
+		stop = true;
+		for (int i = 0; i<5; i++)
+		{
+			if (state[i][3] == 0 && state[i][0]<clock&&state[i][2]==1)
+			{
+				time += clock - state[i][0];
+				state[i][3] = -1;
+			}
+		}
+		clock++;
+		update();
+	}
+
+	if (stop)
+	{
+		cout << temp << "时，电梯停靠在" << floor << "层" << endl;
+	}
+}
+
+
+// 判断该时刻是否有请求
+bool iswaiting()
+{
+	int i = 10;
+	for (i = 0; i < 5; i++)
+	{
+		if (state[i][3] == 1)
+			return true;
+	}
+	if (i == 5)
+		return false;
+}
+
+bool callCheck(int floor,int flag)
+{
+	int i = 0;
+	if (flag == 1)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (state[i][3] == 1 && state[i][1]<floor)
+				return false;
+		}
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (state[i][3] == 1 && state[i][1]>floor)
+				return false;
+		}
+	}
+		return true;
+}
 void Decision::toDeside(int request[][3])
 {
-	int time = 0;
-	int clock=0;         // 模拟时钟
-	int waiting[11] = { 0 };
-	int direction = 0;  // 当前电梯方向
-	int floor=1;        // 当前电梯所在层数
-	int arrived[5] = { 0 };
-	int upNum = 0, dwNum = 0;
-	int upCost = 0, dwCost = 0;
-	
+	int floor = 1;        // 当前电梯所在层数
 	// 按请求时刻从小到大进行排序
 	sort(request);
 
-	while (!isfinished(arrived))
+	for (int i = 0; i < 5; i++)
 	{
-		if (iswaiting(waiting))
+		state[i][3] = 0;
+		for (int j = 0; j < 3; j++)
+			state[i][j] = request[i][j];
+	}
+
+	while (!isfinished())
+	{
+		if (iswaiting()||upNum+dwNum>0)
 		{
-			upCost = (dwNum + upNum)*(10 - floor) + 9 * dwNum;
-			dwCost= (dwNum + upNum)*(floor-1) + 9 * upNum;
-			for (int i = 1; i < 11; i++)
-			{
-				switch (waiting[i])
-				{
-				case 1:
-					if (i > floor)
-					{
-						upCost += 10 - floor;
-						dwCost += floor + 8;
-					}
-					else if (i < floor)
-					{
-						upCost += 30-2*i-floor;
-						dwCost += floor + 8;
-					}
-					break;
-				case -1:
-					if (i > floor)
-					{
-						upCost += 19 - floor;
-						dwCost += 2*i-3+floor;
-					}
-					else if (i < floor)
-					{
-						upCost += 19 - floor;
-						dwCost += floor -1;
-					}
-					break;
-				case 0:
-					break;
-				}
-			}
+			isStop(floor);
 			
-			if (upCost <= dwCost||dwCost==0)
+			if ((dwNum==0&&callCheck(floor,1))||(floor==1&&upNum>0))
 			{
 				floor++;
 				clock++;
-				update(request, waiting, clock);
-				if (waiting[floor] != 0)
-				{
-					if (waiting[floor] == 1)
-					{
-						upNum++;
-						cout << clock << "时，电梯停靠在" << floor << "层" << endl;
-						waiting[floor] = 2;
-						if (floor != 10)
-						{
-							clock++;
-							update(request, waiting, clock);
-						}
-					}
-					else if (waiting[floor] == -1)
-					{
-						dwNum++;
-						cout << clock << "时，电梯停靠在" << floor << "层" << endl;
-						waiting[floor] = -2;
-						if (floor != 1)
-						{
-							clock++;
-							update(request, waiting, clock);
-						}
-					}
-				}
-				if (floor == 10)
-				{
-					upNum = 0;
-					for (int j = 1; j < 11; j++)
-					{
-						if (waiting[j] == 2)
-						{
-							for (int k = 0; k < 5; k++)
-							{
-								if (request[k][1] == j)
-								{
-									time += clock - request[k][0];
-									arrived[k] = 1;
-								}
-							}
-						}
-					}
-					cout <<clock<< "时，电梯停靠在" <<floor<< "层" << endl;
-					clock++;
-					update(request, waiting, clock);
-				}
+				update();
 			}
-			else if(upCost > dwCost || upCost == 0)
+			else if ((upNum == 0 && callCheck(floor, 0))||(floor==10&&dwNum>0))
 			{
 				floor--;
 				clock++;
-				update(request, waiting, clock);
-				if (waiting[floor] != 0)
+				update();
+			}
+			else
+			{
+				upCost = (dwNum + upNum)*(10 - floor) + 9 * dwNum;
+				dwCost = (dwNum + upNum)*(floor - 1) + 9 * upNum;
+
+				for (int i = 0; i < 5; i++)
 				{
-					if (waiting[floor] == 1)
+					if (state[i][3] == 1)
 					{
-						upNum++;
-						cout << clock << "时，电梯停靠在" << floor << "层" << endl;
-						waiting[floor] = 2;
-						if (floor != 10)
+						switch (state[i][2])
 						{
-							clock++;
-							update(request, waiting, clock);
-						}
-					}
-					else if (waiting[floor] == -1)
-					{
-						dwNum++;
-						cout << clock << "时，电梯停靠在" << floor << "层" << endl;
-						waiting[floor] = -2;
-						if (floor != 1)
-						{
-							clock++;
-							update(request, waiting, clock);
+						case 0:
+							if (state[i][1] > floor)
+							{
+								upCost += 10 - floor;
+								dwCost += floor + 8;
+							}
+							else if (state[i][1] < floor)
+							{
+								upCost += 30 - 2 * i - floor;
+								dwCost += floor + 8;
+							}
+							break;
+						case 1:
+							if (state[i][1] > floor)
+							{
+								upCost += 19 - floor;
+								dwCost += 2 * i - 3 + floor;
+							}
+							else if (state[i][1] < floor)
+							{
+								upCost += 19 - floor;
+								dwCost += floor - 1;
+							}
+							break;
+						default:
+							break;
 						}
 					}
 				}
-				if (floor == 1)
+				if (upCost <= dwCost)
 				{
-					dwNum = 0;
-					for (int j = 1; j < 11; j++)
-					{
-						if (waiting[j] == -2)
-						{
-							for (int k = 0; k < 5; k++)
-							{
-								if (request[k][1] == j)
-								{
-									time += clock - request[k][0];
-									arrived[k] = 1;
-								}
-							}
-						}
-					}
-					cout << clock << "时，电梯停靠在" << floor << "层" << endl;
+					floor++;
 					clock++;
-					update(request, waiting, clock);
+					update();
+				}
+				else if (upCost > dwCost)
+				{
+					floor--;
+					clock++;
+					update();
 				}
 			}
 		}
 		else
 		{
 			clock++;
-			update(request, waiting, clock);
+			update();
 		}
 	}
-	cout << time << endl;
+	cout << "总等待时间为："<<time << endl;
 }
 
 
